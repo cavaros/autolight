ba829900d72718c5af79d2a521be2a1adae9847b
Lucas <lucas.camargobatista@icloud.com>
2024-04-16 15:06:14 -0300

    update install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+sudo cp autolight.service /etc/systemd/user
+systemctl --user enable autolight
+systemctl --user start autolight
